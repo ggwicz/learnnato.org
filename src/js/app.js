404f5dff3b3fa186b17a05e29e5fe48b1346bb5e
George Gecewicz <875201+ggwicz@users.noreply.github.com>
2020-05-25 13:37:16 -0400

    be a dummy and realize that the issue is es6 class support not the iOS bug i thought it was re: click events

--- src/js/app.js
+++ src/js/app.js
@@ -5,80 +5,27 @@
  *
  * @author George Gecewicz
  */
-class LearnNato {
 
-    /**
-     * Game DOM elements.
-     *
-     * @since 1.0.0
-     *
-     * @var {object}
-     */
-    els = {};
+const LearnNato = {
 
     /**
      * Constructor.
      *
      * @since 1.0.0
-     *
-     * @var {object} gameEl DOM element for game container.
      */
-    constructor( gameEl ) {
-        this.els = {
-            game: gameEl
-        }
-    }
-
-    /**
-     * Init the game.
-     *
-     * @since 1.0.0
-     */
-    init() {
-        this.bindClicks();
-    }
-
-    /**
-     * Init game's main click event handlers.
-     *
-     * @since 1.0.0
-     */
-    bindClicks() {
-        const buttons = document.querySelectorAll( 'button' );
-
-        buttons.forEach( button => {
-            button.addEventListener( 'click', this.test );
-            button.addEventListener( 'touchstart', this.test );
+    init: () => {
+        document.getElementById( 'foo' ).addEventListener( 'click', event => {
+            alert( 'hi' );
         } );
-        // document.addEventListener( '' )
-        // document.addEventListener( 'click', event => {
-        //     if ( 'undefined' === typeof event.target.dataset.action ) {
-        //         return;
-        //     }
-
-        //     this.setGameState( event.target.dataset.action );
-        // } );
-
-        // document.querySelector( '.intro__button' ).addEventListener( 'touchstart', event => {
-        //     if ( 'undefined' === typeof event.target.dataset.action ) {
-        //         return;
-        //     }
-
-        //     this.setGameState( event.target.dataset.action );
-        // } );
-    }
-
-    test( ) {
-        alert( 'hi' )
-    }
+    },
 
     /**
      * Init game's main click event handlers.
      *
      * @since 1.0.0
      */
-    setGameState( stateName ) {
+    setGameState: ( stateName ) => {
         this.els.game.dataset.activeState = stateName;
     }
 
-}
+};
