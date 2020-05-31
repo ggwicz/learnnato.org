b30b08976d323e250e040823cf7bee8fd2a37da5
George Gecewicz <875201+ggwicz@users.noreply.github.com>
2020-05-31 13:10:08 -0400

    get grid of chracters to show up

--- src/js/app.js
+++ src/js/app.js
@@ -6,13 +6,37 @@
 
 const LearnNato = {
 
+    /**
+     * Primary game container.
+     */
     gameEl: document.querySelector( '.game' ),
 
+    /**
+     * Primary results container.
+     */
+    resultsEl: document.querySelector( '.results' ),
+
+    /**
+     * Set a game-level state.
+     *
+     * @param {string} stateName State to set.
+     */
     setGameState: ( stateName ) => {
         LearnNato.gameEl.dataset.activeState = stateName;
     },
 
+    /**
+     * Init the game.
+     */
     init: () => {
+        LearnNato.initButtons();
+        LearnNato.renderResults();
+    },
+
+    /**
+     * Init button click handlers, which just set game state to data-action value.
+     */
+    initButtons: () => {
         document.querySelectorAll( 'button' ).forEach( button => {
             button.addEventListener( 'click', event => {
                 if ( 'undefined' === typeof event.target.dataset.action ) {
@@ -22,5 +46,17 @@ const LearnNato = {
                 LearnNato.setGameState( event.target.dataset.action );
             } );
         } );
+    },
+
+    /**
+     * Render results.
+     */
+    renderResults: () => {
+        Object.keys( window.nato ).forEach( character => {
+            LearnNato.resultsEl.innerHTML += `<span class="result" data-character="${character}">${character.toUpperCase()}</span>`;
+            window.nato[ character ];
+            console.log( 'character', window.nato[ character ] );
+        } );
     }
+
 };
---
+++ src/js/nato.js
@@ -0,0 +1,90 @@
+window.nato = {
+    'a': [
+        'alpha',
+        'alfa'
+    ],
+    'b': [
+        'bravo'
+    ],
+    'c': [
+        'charlie',
+        'charly'
+    ],
+    'd': [
+        'delta'
+    ],
+    'e': [
+        'echo'
+    ],
+    'f': [
+        'foxtrot',
+        'fox-trot',
+        'fox trot'
+    ],
+    'g': [
+        'golf'
+    ],
+    'h': [
+        'hotel'
+    ],
+    'i': [
+        'india'
+    ],
+    'j': [
+        'juliett',
+        'juliet',
+        'juliette'
+    ],
+    'k': [
+        'kilo'
+    ],
+    'l': [
+        'lima'
+    ],
+    'm': [
+        'mike',
+        'mic'
+    ],
+    'n': [
+        'november'
+    ],
+    'o': [
+        'oscar'
+    ],
+    'p': [
+        'papa'
+    ],
+    'q': [
+        'quebec'
+    ],
+    'r': [
+        'romeo'
+    ],
+    's': [
+        'sierra'
+    ],
+    't': [
+        'tango'
+    ],
+    'u': [
+        'uniform'
+    ],
+    'v': [
+        'victor',
+        'viktor'
+    ],
+    'w': [
+        'whiskey'
+    ],
+    'x': [
+        'xray',
+        'x-ray',
+        'x ray'
+    ],
+    'y': [
+        'yankee'
+    ],
+    'z': [
+        'zulu'
+    ]
+};
