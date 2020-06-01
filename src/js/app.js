5b1805327abfc230db2e25b6edaf8ffe63159e2c
George Gecewicz <875201+ggwicz@users.noreply.github.com>
2020-05-31 22:20:55 -0400

    flesh out dynamic character selection and being handling of code word guess submissions

--- src/js/app.js
+++ src/js/app.js
@@ -7,22 +7,12 @@
 const LearnNato = {
 
     /**
-     * Primary game container.
+     * Primary game els.
      */
-    gameEl: document.querySelector( '.game' ),
-
-    /**
-     * Primary results container.
-     */
-    resultsEl: document.querySelector( '.results' ),
-
-    /**
-     * Set a game-level state.
-     *
-     * @param {string} stateName State to set.
-     */
-    setGameState: ( stateName ) => {
-        LearnNato.gameEl.dataset.activeState = stateName;
+    els: {
+        game: document.querySelector( '.game' ),
+        play: document.querySelector( '.play' ),
+        results: document.querySelector( '.results' )
     },
 
     /**
@@ -30,11 +20,59 @@ const LearnNato = {
      */
     init: () => {
         LearnNato.initButtons();
+        LearnNato.initHandlers();
         LearnNato.renderResults();
     },
 
     /**
-     * Init button click handlers, which just set game state to data-action value.
+     * Set a game-level state.
+     *
+     * @param {string} stateName State to set.
+     */
+    setGameState: ( stateName ) => {
+        LearnNato.els.game.dataset.activeState = stateName;
+
+        if ( 'play' === stateName ) {
+            LearnNato.startGameLoop();
+        }
+    },
+
+    /**
+     * Start game loop.
+     */
+    startGameLoop: () => {
+        LearnNato.setActiveCharacter();
+    },
+
+    /**
+     * Set the game's active game character.
+     */
+    setActiveCharacter: () => {
+        const nextUnsolved = LearnNato.getNextUnsolvedCharacter();
+
+        if ( null === nextUnsolved || undefined == nextUnsolved ) {
+            LearnNato.setGameState( 'victory' );
+        } else {
+            LearnNato.els.game.dataset.activeCharacter = nextUnsolved;
+            LearnNato.els.play.querySelector( 'label' ).textContent = LearnNato.els.game.dataset.activeCharacter.toUpperCase();
+        }
+    },
+
+    /**
+     * Get the next unsolved character in the list.
+     */
+    getNextUnsolvedCharacter: () => {
+        for ( const character in window.nato ) {
+            if ( false === window.nato[ character ].success ) {
+                return character;
+            }
+        }
+
+        return null;
+    },
+
+    /**
+     * Init button click handlers, which mostly just set game state to data-action value.
      */
     initButtons: () => {
         document.querySelectorAll( 'button' ).forEach( button => {
@@ -46,6 +84,25 @@ const LearnNato = {
                 LearnNato.setGameState( event.target.dataset.action );
             } );
         } );
+
+        document.querySelector( '.submit' ).addEventListener( 'click', event => {
+            event.preventDefault();
+            LearnNato.handleGuessSubmission();
+        } );
+    },
+
+    /**
+     * Handle submitting a guess at a character's code word.
+     */
+    handleGuessSubmission: () => {
+        // get the current character's code words
+        // compare submission to those values
+        // if match, success
+        // - update corresponding "result" icon for character
+        // - load next unsolved character
+        // if no match, failure
+        // - update corresponding "result" icon for character
+        // - stay on character, clear input, say "incorrect, try again".
     },
 
     /**
@@ -53,9 +110,8 @@ const LearnNato = {
      */
     renderResults: () => {
         Object.keys( window.nato ).forEach( character => {
-            LearnNato.resultsEl.innerHTML += `<span class="result" data-character="${character}">${character.toUpperCase()}</span>`;
-            window.nato[ character ];
-            console.log( 'character', window.nato[ character ] );
+            LearnNato.els.results.innerHTML += `<span class="result" data-character="${character}">${character.toUpperCase()}</span>`;
+            window.nato[ character ].success = false;
         } );
     }
 
